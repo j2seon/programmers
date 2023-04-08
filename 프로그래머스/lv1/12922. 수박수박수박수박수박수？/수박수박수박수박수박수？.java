332671a7356ea06c05f10cdec34074fccdffcb8a
Ju Jinseon <97584505+j2seon@users.noreply.github.com>
2023-04-08 13:38:39 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.08 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int n) {
+        StringBuffer sb = new StringBuffer();
+        
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 0){
+                sb.append("박");
+            }else{
+                sb.append("수");
+            }
+        }
+        return sb.toString();
+    }
+}
