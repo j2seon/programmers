f5de2fbc2fff7759f65a449d0ce48796d7523f01
Ju Jinseon <97584505+j2seon@users.noreply.github.com>
2023-09-19 21:13:18 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String str) {
+        StringBuilder bulider = new StringBuilder(str);
+        return bulider.reverse().toString();
+    }
+}
